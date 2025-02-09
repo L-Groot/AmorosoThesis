bad23744d7fdc7d7430bfe0a8e156a557d51e029
Laura Groot <lauragroot@MacBook-Pro-von-Laura.local>
2025-02-09 17:12:15 +0100

    -

--- more_figures_for_paper.R
+++ more_figures_for_paper.R
@@ -1,4 +1,49 @@
 # More figures for paper
+source(paste0("https://raw.githubusercontent.com/L-Groot/AmorosoThesis/refs/",
+              "heads/main/estimate_methods.R"))
+
+#-------------------------
+# (1) Bimodal geyser data
+#-------------------------
+
+geyser_dat <- multimode::geyser
+geyser_res <- estimate_methods(geyser)
+plot_methods(geyser_dat, geyser_res)
+
+
+
+#----------------------
+# (2) Unimodal RT data
+#----------------------
+
+# Data from subject number 1 on the 
+s01gs <- read.table("noisedat/S01GS.DAT") %>%
+  # Rename columns
+  rename(
+    key = V1,
+    rt = V2,
+    ignore = V3,
+    odd = V4,
+    rsi = V5,
+    stim = V6
+  ) %>%
+  # Remove rows with rt < 500ms or > 2500ms
+  filter(rt >= 500 & rt <= 2500)
+
+
+rt_dat <- s01gs
+rt_res <- estimate_methods(rt_dat)
+
+
+#--------------------------------
+# (3) Unimodal MCMC samples data
+#--------------------------------
+
+
+
+
+
+
 
 bs_and_adjKDE <- function(data, breaks = 20) {
   
