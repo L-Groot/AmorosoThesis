1497d9555a9ef9cf3782012c498d4b337bbd20a2
Laura Groot <lauragroot@re-byodm-145-109-104-247.wireless.uva.nl>
2025-03-15 19:18:51 +0100

    -

--- investigate_mixed_normal.R
+++ investigate_mixed_normal.R
@@ -1,10 +1,31 @@
 par_amo1 <- c(a = 5, l = 0.3, c = 5, mu = 0)
 par_exgauss1 <- c(1, 0.1, 2)
-
-n <- 200
-dat <- rexGAUS(n,mu=par[1],sigma=par[2],nu=par[3])
-
 par <- par_exgauss1
+
+n <- 2000
+set.seed(40)
+dat <- rexGAUS(n,mu=par[1],sigma=par[2],nu=par[3])
+length(dat)
+
+increments <- seq(10,2000,by=100)
+
+df <- data.frame(n = increments, nr_components = numeric(20))
+
+for (i in 1:length(increments)) {
+  
+  x <- increments[i]
+  print(x)
+  data <- dat[1:x]
+  print(length(data))
+  res <- estimate_methods(data)
+  plot_some_methods(data, res, method_to_plot = "mnorm", lwd = 1.5, legend = F)
+  #readline(prompt = "Press [Enter] to continue to the next iteration...")
+  res_mnorm <- densityMclust(data, plot = TRUE)
+  df$nr_components[i] <- res_mnorm$G
+  #readline(prompt = "Press [Enter] to continue to the next iteration...")
+  
+}
+
 
 
 res <- densityMclust(dat, G = 2, plot = T)
